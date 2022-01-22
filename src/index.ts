3ad990c9c8f560bb195e1789e6c0f46c9f189be8
whoyoux <48690153+whoyoux@users.noreply.github.com>
2022-01-22 19:09:18 +0100

    Add error check if user provide any args or not

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,12 @@ export type createArrayProps = {
   step?: number;
 };
 
-export const createArray = ({ from = 0, step = 1, to }: createArrayProps): number[] => {
-  if (!!!to) {
-    throw new Error('Please provide how many elements shoud be in the array!');
-  }
+export const createArray = (args: createArrayProps): number[] => {
+  if (!!!args) throw new Error('Please provide args!');
+
+  const { from = 0, step = 1, to } = args;
+
+  if (!!!to) throw new Error('Please provide how many elements shoud be in the array!');
 
   return Array.from(
     {
